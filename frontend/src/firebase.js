d190cd8e55a78134feaaeb071bd64b10b12011e2
Nisitha Sree Gadhi <nisithasreeg@outlook.com>
2026-05-16 13:09:39 -0500

    basic log-in screen front end is set need to change theme and add dashboard features

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,11 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBJ8mHY6xGVfvwF3za8KU6Ev4BeCh4tS7c",
   authDomain: "agra-21e39.firebaseapp.com",
@@ -18,4 +20,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export {
+  auth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+};
